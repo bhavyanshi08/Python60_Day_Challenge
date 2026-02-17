feca595c2a2b549535fb7ae919d9b40e447fdde0
bhavyanshi08 <bhavyanshi.earla@gmail.com>
2026-02-17 19:45:49 +0530

    Initial program to take input and classify package weights

---
+++ Day-5.py
@@ -0,0 +1,23 @@
+Name = "Earla Bhavyanshi"
+L = len(Name) - Name.count(" ")
+PLI = L % 3
+N = int(input("Enter Package weights: "))
+weights = [0] * N
+for i in range(0, N):
+    weights[i] = int(input("weight-{i+1}: "))
+very_light = []
+normal_load = []
+heavy_load = []
+overload = []
+invalid_entries = []
+for i in range(N):
+    if weights[i] < 0:
+        invalid_entries += [weights[i]]
+    elif weights[i] < 6:
+        very_light += [weights[i]]
+    elif weights[i] < 26:
+        normal_load += [weights[i]]
+    elif weights[i] < 61:
+        heavy_load += [weights[i]]
+    else:
+        overload += [weights[i]]
