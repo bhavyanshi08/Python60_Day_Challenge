edb10a60127d7f6f0f24679302886123fc762a96
bhavyanshi08 <bhavyanshi.earla@gmail.com>
2026-02-17 19:50:59 +0530

    Added final output display and completed smart transport load balancing program

--- Day-5.py
+++ Day-5.py
@@ -1,15 +1,19 @@
 Name = "Earla Bhavyanshi"
 L = len(Name) - Name.count(" ")
 PLI = L % 3
+
 N = int(input("Enter Package weights: "))
 weights = [0] * N
+
 for i in range(0, N):
-    weights[i] = int(input("weight-{i+1}: "))
+    weights[i] = int(input(f"weight-{i+1}: "))
+
 very_light = []
 normal_load = []
 heavy_load = []
 overload = []
 invalid_entries = []
+
 for i in range(N):
     if weights[i] < 0:
         invalid_entries += [weights[i]]
@@ -21,9 +25,21 @@ for i in range(N):
         heavy_load += [weights[i]]
     else:
         overload += [weights[i]]
+
 valid = len(normal_load) + len(heavy_load) + len(overload)
 affected = 0
+
 if PLI == 0:
     invalid_entries += overload
     affected = len(overload)
     overload = []
+
+print("\nNo. of Valid Weights:", valid)
+print("No. of Affected Items:", affected)
+print("L =", L, "and PLI =", PLI)
+
+print("---Final Lists---")
+print("Normal Load:", normal_load)
+print("Heavy Load:", heavy_load)
+print("Overload:", overload)
+print("Invalid Entries:", invalid_entries)
